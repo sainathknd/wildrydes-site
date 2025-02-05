0db177a4121e2975dad4f10068aea760032e63d2
sainathknd <107765719+sainathknd@users.noreply.github.com>
2025-02-05 16:44:19 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_VlBDGSnXi', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'jk72g8en51rt0qeiucrrg5i45', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
